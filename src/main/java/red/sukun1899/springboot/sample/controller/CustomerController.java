4c74c8cee7d9281f9b64c5854dc5dc9eb0df82e1
su-kun1899 <higedrum.coz@gmail.com>
2017-06-07 00:37:35 +0900

    Add create method to customer controller

--- src/main/java/red/sukun1899/springboot/sample/controller/CustomerController.java
+++ src/main/java/red/sukun1899/springboot/sample/controller/CustomerController.java
@@ -1,12 +1,19 @@
 package red.sukun1899.springboot.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import red.sukun1899.springboot.sample.entity.Customer;
 import red.sukun1899.springboot.sample.service.CustomerService;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 /**
@@ -26,5 +33,44 @@ public class CustomerController {
         List<Customer> customers = customerService.findAll();
         model.addAttribute("customers", customers);
         return "customers/list";
+    }
+
+    @PostMapping(value = "create")
+    public String create(@ModelAttribute @Validated CustomerForm customerForm,
+                         BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return list(model);
+        }
+        Customer customer = new Customer();
+        BeanUtils.copyProperties(customerForm, customer);
+        customerService.create(customer);
+
+        return "redirect:/customers";
+    }
+
+    public class CustomerForm {
+        @NotNull
+        @Size(min = 1, max = 127)
+        private String firstName;
+
+        @NotNull
+        @Size(min = 1, max = 127)
+        private String lastName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
     }
 }

593052a52773d005086911e2b34fa067a3601e56
su-kun1899 <higedrum.coz@gmail.com>
2017-07-22 17:12:14 +0900

    Enable to work

--- run-heroku.sh
+++ run-heroku.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+readonly ENV_PORT=${PORT}
+
 # postgres://<username>:<password>@<hostname>:<port>/<path>
 readonly POSTGRES_DATABASE_URL=${DATABASE_URL}
 
@@ -13,9 +15,11 @@ POSTGRES_USER_INFO=${POSTGRES_USER_INFO%%@*}
 echo ${POSTGRES_USER_INFO}
 
 readonly POSTGRES_USER=`echo ${POSTGRES_USER_INFO} | cut -d ':' -f 1`
-readonly POSTGRES_PASSWORD=`echo ${POSTGRES_USER_INFO} | cut -d ':' -f 1`
+readonly POSTGRES_PASSWORD=`echo ${POSTGRES_USER_INFO} | cut -d ':' -f 2`
 
 echo ${POSTGRES_USER}
 echo ${POSTGRES_PASSWORD}
 
-java -jar target/springboot-web-sample-0.0.1-SNAPSHOT.jar --server.port=${PORT}
+exit 0
+
+java -jar target/springboot-web-sample-0.0.1-SNAPSHOT.jar --server.port=${ENV_PORT}

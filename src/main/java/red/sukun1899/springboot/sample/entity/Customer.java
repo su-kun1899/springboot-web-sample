1353cff63b3bb308492d9e83576e139779fa1108
su-kun1899 <higedrum.coz@gmail.com>
2017-07-22 18:01:43 +0900

    Use postgres mode

--- src/main/java/red/sukun1899/springboot/sample/entity/Customer.java
+++ src/main/java/red/sukun1899/springboot/sample/entity/Customer.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Table(name = "customers")
 public class Customer {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false)

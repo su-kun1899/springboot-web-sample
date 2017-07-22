fe306bb13a31fd78bf43698afe036da378c9c71c
su-kun1899 <higedrum.coz@gmail.com>
2017-07-22 17:22:31 +0900

    Remove debug comment

--- run-heroku.sh
+++ run-heroku.sh
@@ -12,13 +12,8 @@ POSTGRES_USER_INFO=${POSTGRES_DATABASE_URL##postgres://}
 # -> <username>:<password>
 POSTGRES_USER_INFO=${POSTGRES_USER_INFO%%@*}
 
-echo ${POSTGRES_USER_INFO}
-
 readonly POSTGRES_USER=`echo ${POSTGRES_USER_INFO} | cut -d ':' -f 1`
 readonly POSTGRES_PASSWORD=`echo ${POSTGRES_USER_INFO} | cut -d ':' -f 2`
-
-echo ${POSTGRES_USER}
-echo ${POSTGRES_PASSWORD}
 
 java -jar target/springboot-web-sample-0.0.1-SNAPSHOT.jar \
     --server.port=${ENV_PORT} \

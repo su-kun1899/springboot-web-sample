518ea488c3f9b5f73db88ae3a03806aacf6a3c6c
su-kun1899 <higedrum.coz@gmail.com>
2017-07-29 14:55:05 +0900

    Add integration test

--- src/test/java/red/sukun1899/springboot/sample/controller/CustomerControllerIntegrationTest.java
+++ src/test/java/red/sukun1899/springboot/sample/controller/CustomerControllerIntegrationTest.java
@@ -1,13 +1,22 @@
 package red.sukun1899.springboot.sample.controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import red.sukun1899.springboot.sample.SpringbootWebSampleApplication;
+import red.sukun1899.springboot.sample.service.CustomerService;
 
-import static org.junit.Assert.*;
+import static com.sun.org.apache.xerces.internal.util.PropertyState.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author su-kun1899
@@ -19,5 +28,13 @@ import static org.junit.Assert.*;
 @AutoConfigureMockMvc
 @ActiveProfiles("it")
 public class CustomerControllerIntegrationTest {
+    @Autowired
+    private MockMvc mvc;
 
+    @Test
+    @WithMockUser
+    public void whenGetCustomers_thenStatus200() throws Exception {
+        mvc.perform(get("/customers"))
+                .andExpect(status().isOk());
+    }
 }
